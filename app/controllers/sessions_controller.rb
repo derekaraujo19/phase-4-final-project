fe79cfcd602ac8e2e9c55b1b8727703918b8f445
Derek Araujo <derekaraujo19@gmail.com>
2022-10-04 00:00:58 -0400

    First commit- user model and routes implemented

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+  skip_before_action :authorize, only: :create
+
+  #Log In
+  def create
+    # finder user by username
+    user = User.find_by(username: :params[:username])
+    # if user and authenticated password exists
+    if user&.authenticate(params[:password])
+      # set id on session hash
+      session[:user_id] = user.id
+      # return json response
+      render json: user
+    else
+      # if user doesn't exist and password isn't authenticated
+      render json: {error: "Invalid username or password"}, status: :unauthorized
+    end
+  end
+
+
+  # Log Out
+  def destroy
+    session.delete :user_id
+    head :no_content
+  end
+
+
+
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :username, presence: true, uniqueness: true
+
+  # has_many :reviews
+  # has_many :albums, through: :reviews
+
+end
