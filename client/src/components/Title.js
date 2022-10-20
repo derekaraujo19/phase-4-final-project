972230e96007bd663cd2c3b7759d624730bbe7b5
Derek Araujo <derekaraujo19@gmail.com>
2022-10-19 22:23:14 -0400

    My Reviews list styled

--- client/src/components/Title.js
+++ client/src/components/Title.js
@@ -1,8 +1,9 @@
 function Title(){
   return (
     <div className="Title">
-      <h5 id="subtitle">hot takes of your favorite albums</h5>
+      <h5 className="subtitle">hot takes of your favorite albums</h5>
       <h1 id="title">DiscSnob</h1>
+      {/* <h5 className="subtitle" style={{color:"#f5f5f5"}}>in 250 characters or less</h5> */}
     </div>
   );
 }
